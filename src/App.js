cbe0481406f0ebb2bbe46e651af3c49d6f099e5d
wan8234@naver.com <you@example.com>
2020-09-02 17:25:51 +0900

    Delete and clonecoding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}){
+  return <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image: "https://gbc-cdn-public-media.azureedge.net/img64004.1426x713.jpg"
+  },
+  {
+    id: 2,
+    name: "ramen",
+    image: "http://glebekitchen.com/wp-content/uploads/2018/08/koreanramenfront.jpg"
+  },
+  {
+    id: 3,
+    name: "Samgyeopsal",
+    image: "https://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg"
+  },
+  {
+    id: 4,
+    name: "Bibimbap",
+    image: "https://img.huffingtonpost.com/asset/5727ef362200003600255394.jpeg?cache=XowoERt6W4&ops=1910_1000"
+  },
+  {
+    id: 5,
+    name: "Doncasu",
+    image: "https://seonkyounglongest.com/wp-content/uploads/2017/07/donkasu-02-1000x667.jpg?x61413"
+  },
+  {
+    id: 6,
+    name: "Kimbap",
+    image: "https://i.ytimg.com/vi/Y-Y9CXGRJPU/maxresdefault.jpg"
+  }
+]  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => {
+        return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      })}
     </div>
   );
 }
