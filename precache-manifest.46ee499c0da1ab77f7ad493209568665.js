36474775a45c182c124706a7efdda81e5d2b8c03
wan8234@naver.com <you@example.com>
2020-09-04 09:52:01 +0900

    Updates

---
+++ precache-manifest.46ee499c0da1ab77f7ad493209568665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5bb4f875fbc81bda9922c3eb3892b1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a384887869a2be2978ca",
+    "url": "/movie_app_2020/static/css/main.3147e5d1.chunk.css"
+  },
+  {
+    "revision": "41f9863971635899e0a4",
+    "url": "/movie_app_2020/static/js/2.7a601478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.7a601478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a384887869a2be2978ca",
+    "url": "/movie_app_2020/static/js/main.4fdbaaa3.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
